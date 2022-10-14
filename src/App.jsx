6f3e954b0679359cdf73e383ca72228df9613504
navargas29 <navargas291194@gmail.com>
2022-10-14 11:35:30 -0600

    se agregan comentarios

---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import FormUsers from "./components/FormUsers";
+import UserCard from "./components/UserCard";
+
+const baseURL = "http://144.126.218.162:9000";
+
+function App() {
+  const [users, setUsers] = useState();
+  // Esto es parara pasar info desde UserCard hasta FormUser
+  const [updateInfo, setUpdateInfo] = useState();
+
+  // Este es para hacer el get de todos los users
+  const getAllUsers = () => {
+    const URL = `${baseURL}/users/`;
+    axios
+      .get(URL)
+      .then((res) => setUsers(res.data))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
+  //para crear un nuevo usuario
+  const createNewUser = (data) => {
+    const URL = `${baseURL}/users/`;
+    axios
+      .post(URL, data)
+      .then((res) => {
+        console.log(res.data);
+        getAllUsers();
+      })
+      .catch((err) => console.log(err));
+  };
+
+  // Para eliminar un usuario especifico
+  const deleteUserById = (id) => {
+    const URL = `${baseURL}/users/${id}/`;
+    axios
+      .delete(URL)
+      .then((res) => {
+        console.log(res.data);
+        getAllUsers();
+      })
+      .catch((err) => console.log(err));
+  };
+
+  // Para actualizar un usuario especifico
+  const updateUserById = (id, data) => {
+    const URL = `${baseURL}/users/${id}/`;
+    axios
+      .patch(URL, data)
+      .then((res) => {
+        console.log(res.data);
+        getAllUsers();
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="App">
+      <h1>Users CRUD</h1>
+      <FormUsers
+        createNewUser={createNewUser}
+        updateInfo={updateInfo}
+        updateUserById={updateUserById}
+        setUpdateInfo={setUpdateInfo}
+      />
+      {users?.map((user) => (
+        <UserCard
+          key={user.id}
+          user={user}
+          deleteUserById={deleteUserById}
+          setUpdateInfo={setUpdateInfo}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./styles/userCard.css";
+const UserCard = ({ user, deleteUserById, setUpdateInfo }) => {
+  const handleEdit = () => {
+    setUpdateInfo(user);
+  };
+
+  return (
+    <article className="user">
+      <h2 className="user_name">{`${user.first_name} ${user.last_name}`}</h2>
+      <ul className="user_list">
+        <li className="user_item">
+          <span className="user_span">Email</span>
+          {user.email}
+        </li>
+        <li className="user_item">
+          <span className="user_span">Birthday</span>
+          {user.birthday}
+        </li>
+      </ul>
+      <footer className="user_footer">
+        <button className="user_btn" onClick={() => deleteUserById(user.id)}>
+          <i className="fa-solid fa-trash-can"></i>
+        </button>
+
+        <button className="user_btn" onClick={handleEdit}>
+          <i className="user_edit fa-sharp fa-solid fa-pencil"></i>
+        </button>
+      </footer>
+    </article>
+  );
+};
+
+export default UserCard;
